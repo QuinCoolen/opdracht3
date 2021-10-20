24983d617630b306d36e3112ffef5c429c122261
Quin Coolen <quin.coolen@gmail.com>
2021-10-20 13:18:05 +0200

    css

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -5,13 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <form action="{{ route('songs.store') }}" method="POST">
-        @csrf
-        <input type="text" name="title" id="title" required>
-        <input type="text" name="singer" id="singer" required>
-        <button type="submit">Submit</button>
-    </form>
+<body class="grid place-items-center h-screen">
+    <div class="flex flex-col text-center gap-4">
+        <h1 class="text-2xl">Create New Song</h1>
+        <form class="flex flex-col gap-4" action="{{ route('songs.store') }}" method="POST">
+            @csrf
+            <input class="flex-shrink w-52 m-auto text-center px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" type="text" name="title" id="title" required>
+            <input class="flex-shrink w-52 m-auto text-center px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" type="text" name="singer" id="singer" required>
+            <button class="flex-shrink w-24 m-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Submit</button>
+        </form>
+    </div>
 </body>
 </html>
--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    {{$song->title}} - {{$song->singer}}
+<body class="overflow-hidden">
+    <button class="m-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="window.location.href = '/songs';">Back</button>
+    <div class="grid place-items-center h-screen">
+        <ul class="border-2 border-black rounded-lg p-3">
+            <li>ID: {{$song->id}}</li>
+            <li>Title: {{$song->title}}</li>
+            <li>Singer: {{$song->singer}}</li>
+        </ul>        
+    </div>
 </body>
 </html>
