56a2e6dde98e000ffbf3b69aeeab21908463d2f4
Quin Coolen <quin.coolen@gmail.com>
2021-10-20 14:03:29 +0200

    responsive

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -12,10 +12,10 @@
         <a class="text-center w-52 m-auto bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" href="/songs/create">Create New Song</a>
     </div>
     <div class="grid gap-4 grid-rows-2 grid-cols-3 content-between">
-        <div class="flex gap-4 p-5 col-span-3">
+        <div class="flex flex-wrap gap-4 p-5 col-span-3">
         @foreach ($songs as $song)
-            <div class="flex-1 border-2 border-black rounded-lg p-3 flex flex-col gap-4">
-                <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex-1" href="/songs/{{$song->id}}">  {{ $song->title }} - {{$song->singer}} </a>
+            <div class="flex-auto border-2 border-black rounded-lg p-3 flex flex-col gap-4">
+                <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex-1" href="/songs/{{$song->id}}">  {{ $song->title }} <br/>By {{$song->singer}} </a>
                 <div>
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex-1" onclick="window.location.href = '/songs/{{$song->id}}/edit';">Edit</button>
                 </div>
--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -8,7 +8,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="overflow-hidden">
-    <button class="m-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="window.location.href = '/songs';">Back</button>
+    <button class="fixed m-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="window.location.href = '/songs';">Back</button>
     <div class="grid place-items-center h-screen">
         <ul class="border-2 border-black rounded-lg p-3">
             <li>ID: {{$song->id}}</li>
